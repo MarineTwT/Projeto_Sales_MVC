89ddd1d950269bdcc17f386f73ecbebccf39c8e2
MarineTwT <pablojdrosa@gmail.com>
2023-10-09 15:26:28 +0100

    First controller and Razer pages tests

--- Projeto_SalesMVC/Controllers/HomeController.cs
+++ Projeto_SalesMVC/Controllers/HomeController.cs
@@ -23,6 +23,13 @@ namespace Projeto_SalesMVC.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
