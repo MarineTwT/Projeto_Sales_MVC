19f18dcaf5247928a0399110059e9b775919eb36
MarineTwT <pablojdrosa@gmail.com>
2023-10-11 16:28:21 +0100

    SellersController created

---
+++ Projeto_SalesMVC/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_SalesMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
